3f395cfb56d5886ad171494f7ffd6f081c591227
kermage <genealyson.torcende@gmail.com>
2024-02-17 22:21:17 +0800

    use new extensions

--- Logger.php
+++ Logger.php
@@ -8,8 +8,8 @@
 namespace ThemePlate;
 
 use Monolog\Logger as BaseLogger;
-use Monolog\Formatter\LineFormatter;
-use Monolog\Handler\RotatingFileHandler;
+use ThemePlate\Logger\Handler;
+use ThemePlate\Logger\Processor;
 
 class Logger {
 
@@ -52,69 +52,16 @@ class Logger {
 	public function channel( string $name, bool $context = false ): BaseLogger {
 
 		if ( ! isset( self::$instances[ $name ] ) ) {
+			$channel = $this->path . DIRECTORY_SEPARATOR . $name . '.log';
+
 			self::$instances[ $name ] = new BaseLogger(
 				$name,
-				array( $this->handler( $name ) ),
-				array( $this->processor( $context ) )
+				array( new Handler( $channel ) ),
+				array( new Processor( $context ) )
 			);
 		}
 
 		return self::$instances[ $name ];
-
-	}
-
-
-	protected function handler( string $channel ): RotatingFileHandler {
-
-		$filename = $this->path . $channel . '.log';
-		$handler  = new RotatingFileHandler(
-			$filename,
-			0,
-			BaseLogger::DEBUG,
-			true,
-			0664,
-			false
-		);
-
-		$handler->setFilenameFormat( '{date}/{filename}', 'Y/m/d' );
-		$handler->setFormatter( $this->formatter() );
-
-		return $handler;
-
-	}
-
-
-	protected function formatter(): LineFormatter {
-
-		$format = "[%datetime%] %level_name% > %message% %context% %extra%\n";
-
-		return new LineFormatter( $format, 'Y-m-d H:i:s', true, true );
-
-	}
-
-
-	protected function processor( bool $context ): callable {
-
-		return static function ( $record ) use ( $context ) {
-
-			$forced = array_key_exists( 'wp', $record['context'] ) ? 'wp' : array_search( 'wp', $record['context'], true );
-
-			if ( $context || false !== $forced ) {
-				$record['extra'] = array_merge(
-					$record['extra'],
-					array(
-						'doing_cron' => defined( 'DOING_CRON' ) && DOING_CRON,
-						'doing_ajax' => defined( 'DOING_AJAX' ) && DOING_AJAX,
-						'is_admin'   => defined( 'WP_ADMIN' ) && WP_ADMIN,
-					)
-				);
-
-				unset( $record['context'][ $forced ] );
-			}
-
-			return $record;
-
-		};
 
 	}
 
